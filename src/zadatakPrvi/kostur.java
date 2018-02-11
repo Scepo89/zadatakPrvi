61753aa3fe01378331c3bcc08e90a64e642769de
Scepo89 <veselinsimic90@gamil.com>
2018-02-11 23:51:08 +0100

    pravljenje kostura

--- src/zadatakPrvi/kostur.java
+++ src/zadatakPrvi/kostur.java
@@ -1,7 +1,21 @@
 package zadatakPrvi;
 
 public class kostur {
-
+public static void primeLessThan100Print(){
+	
+}
+public static void emirps100(){
+	
+}
+public static void fibonaciLessThan100(){
+	
+}
+public static void marsenesPrime(){
+	
+}
+public static void amstrongovBroj(){
+	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
